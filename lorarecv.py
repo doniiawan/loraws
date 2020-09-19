e193733529560b2139f58d78716815288ceb8c5d
doniiawan <doniiawan@gmail.com>
2020-09-19 20:56:24 +0700

    [feat] : add lora recv

---
+++ lorarecv.py
@@ -0,0 +1,33 @@
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+
+BOARD.setup()
+
+class LoRaRcvCont(LoRa):
+    def __init__(self, verbose=False):
+        self.set_freq(915.0)
+        super(LoRaRcvCont, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0] * 6)
+
+    def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        while True:
+            sleep(.5)
+            rssi_value = self.get_rssi_value()
+            status = self.get_modem_status()
+            sys.stdout.flush()
+            
+
+    def on_rx_done(self):
+        print("\nReceived: ")
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=True)
+        print(bytes(payload).decode("utf-8",'ignore'))
+        self.set_mode(MODE.SLEEP)
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT) 
+
+lora = LoRaRcvCont(verbose=False)
+lora.set_mode(MODE.STDBY)
